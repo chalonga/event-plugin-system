2f31943298ae74acc7338f3802dd0e49ac75de0f
los <chalonga@gmail.com>
2024-06-30 10:22:20 -0700

    bump ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="event-plugin-system",
-    version="0.0.4",
+    version="0.0.5",
     author="Carlos",
     author_email="chalonga@gmail.com",
     description="A simple plugin event system",
