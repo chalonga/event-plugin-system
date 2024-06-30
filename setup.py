116317dff3d83f03b1541796a3ccf7570bc9408b
los <chalonga@gmail.com>
2024-06-30 12:37:43 -0700

    test new publish workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="event-plugin-system",
-    version="0.0.7",
+    version="0.0.8",
     author="Carlos",
     author_email="chalonga@gmail.com",
     description="A simple plugin event system",
